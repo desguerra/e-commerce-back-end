bce6e2e1932a56bc0c1a4439fadb666453f27094
desguerra <ddesguerra@ucdavis.edu>
2022-09-25 19:59:51 -0700

    get by id routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,39 +4,68 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  Tag.findAll({
-    include: [
-        {
-            model: Product,
-            attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-            through: ProductTag,
-        },
-    ],
-})
-    .then((dbCatData) => res.json(dbCatData))
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    // find all tags
+    // be sure to include its associated Product data
+    Tag.findAll({
+        include: [
+            {
+                model: Product,
+                attributes: [
+                    'id',
+                    'product_name',
+                    'price',
+                    'stock',
+                    'category_id',
+                ],
+                through: ProductTag,
+            },
+        ],
+    })
+        .then((dbTagData) => res.json(dbTagData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    Tag.findOne({
+        where: {
+            id: req.params.id,
+        },
+        include: [
+            {
+                model: Product,
+                attributes: [
+                    'id',
+                    'product_name',
+                    'price',
+                    'stock',
+                    'category_id',
+                ],
+                through: ProductTag,
+            },
+        ],
+    })
+        .then((dbTagData) => res.json(dbTagData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
 });
 
 module.exports = router;
